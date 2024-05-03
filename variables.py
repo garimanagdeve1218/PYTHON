d72f9266a590dcb776ddae014e5032415c323fda
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-03 22:40:40 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,67 @@
+#without variables.
+print("My name is Garima, ")
+print("I am currently an engineering student.")
+print("My age is 20.")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+My name is Garima, 
+I am currently an engineering student.
+My age is 20.
+
+Process finished with exit code 0
+
+
+
+
+
+
+
+#with variables.
+character_name="Doll"
+character_degree="B.Tech"
+character_age="21"
+
+print("My name is " + character_name + ", ")
+print("I am currently pursuing " + character_degree + ".")
+print("My age is " +character_age + ".")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+My name is Doll, 
+I am currently pursuing B.Tech.
+My age is 21.
+
+
+
+
+
+
+
+#changing the values of the variables in the middle of the program.
+character_name="Doll"
+character_degree="B.Tech"
+character_age="21"
+
+print("My name is " + character_name + ", ")
+print("I am currently pursuing " + character_degree + ".")
+print("My age is " +character_age + ".")
+
+character_name="xyz"
+character_age="25"
+
+print("My nick name is " +character_name + ".")
+print("And people often think I am "+ character_age +" years old.")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+My name is Doll, 
+I am currently pursuing B.Tech.
+My age is 21.
+My nick name is xyz.
+And people often think I am 25 years old.
+
+Process finished with exit code 0
+
+
+
+
+
+Process finished with exit code 0
