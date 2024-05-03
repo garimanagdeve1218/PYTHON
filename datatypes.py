fffa52d5edfe12eb611bd4b21f3f0d1b10c561e1
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-03 22:59:42 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,68 @@
+#string datatype.
+character_name = "Garima"
+
+#integer datatype.
+age = 20
+
+#decimal datatype.
+marks = 98.99
+
+#boolean datatype.
+is_Male = True
+
+
+
+
+
+
+
+
+
+#to know the datatype.
+x = 5
+print(type(x))
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+<class 'int'>
+
+Process finished with exit code 0
+
+
+
+
+
+
+
+
+
+#setting datatypes.
+x = "Hello World"	                                             #str	
+
+x = 20	                                                       #int	
+
+x = 20.5	                                                     #float	
+
+x = 1j	                                                       #complex
+
+x = ["apple", "banana", "cherry"]	                             #list
+
+x = ("apple", "banana", "cherry")	                             #tuple	
+
+x = range(6)	                                                 #range
+
+x = {"name" : "John", "age" : 36}	                             #dictionary	
+
+x = {"apple", "banana", "cherry"}	                             #set	
+
+x = frozenset({"apple", "banana", "cherry"})	                 #frozenset	
+
+x = True	                                                     #bool	
+
+x = b"Hello"	                                                 #bytes
+
+x = bytearray(5)	                                             #bytearray	
+
+x = memoryview(bytes(5))	                                     #memoryview	
+
+x = None	                                                     #NoneType
+
