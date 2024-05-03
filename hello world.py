518a26186f28824b5440dcd244385ce42b6b0e75
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-03 22:27:28 +0530

    Update helloworld.py

--- hello world.py
+++ hello world.py
@@ -1,8 +1,26 @@
 #to write hello world program.
 print("HELLO WORLD!")
 
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+HELLO WORLD!
+
+Process finished with exit code 0
+
+
+
+
+
+
 #to make a triangle.
 print("    /|")
 print("   / |")
 print("  /  |")
 print(" /___|")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+    /|
+   / |
+  /  |
+ /___|
+
+Process finished with exit code 0
