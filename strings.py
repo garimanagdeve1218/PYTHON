7b20e45343f20396e7c96373984af6b58836be4c
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-05 03:57:53 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,37 @@
+#how to write a string.
+print("this is how you write a string")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+this is how you write a string
+
+Process finished with exit code 0
+
+
+
+#or
+print("this\nis how\nyou write a string.")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+this
+is how
+you write a string.
+
+Process finished with exit code 0
+
+
+
+#put quotation mark inside a string.
+print("this is how you \"write\" a string.")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+this is how you "write" a string.
+
+Process finished with exit code 0
+
+
+
+
+
+
+
+
