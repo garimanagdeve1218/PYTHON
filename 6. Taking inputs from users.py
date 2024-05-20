b971fd968b80f49b8bf696607b9314fa1ce91f2c
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-21 00:47:31 +0530

    Create 6. Taking inputs from users.py

---
+++ 6. Taking inputs from users.py
@@ -0,0 +1,10 @@
+#input from user:
+
+name = input("Enter you name:")
+age = input("Enter your age:")
+print("Hello " + name + "! You are " + age)
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+Enter you name:Garima
+Enter your age:20
+Hello Garima! You are 20
