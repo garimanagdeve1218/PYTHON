4d31d18816f47872702f8e768dc7ed50db86d869
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-20 17:55:30 +0530

    Update 4. strings.py

--- 4. strings.py
+++ 4. strings.py
@@ -24,10 +24,29 @@ Process finished with exit code 0
 print("this is how you \"write\" a string.")
 
 "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
-this is how you "write" a string.
+this is how you "write" a 
 
 Process finished with exit code 0
 
+
+
+#string with variables:
+phrase=("My name is Garima Nagdeve.")
+print(phrase)
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+this is how you "write" a 
+
+
+
+#concatination: Appending one with to another string.
+phrase="My name is "
+print(phrase + "Garima.")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+My name is Garima.
+
+Process finished with exit code 0
 
 
 
