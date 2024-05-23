6886915d444412ecd69f1e2c8b94a7f043aec30d
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-24 00:32:09 +0530

    Create 8. Mad Libs Game.py

---
+++ 8. Mad Libs Game.py
@@ -0,0 +1,19 @@
+#Mad Libs Game:
+
+color = input("Enter a Color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a Celebrity: ")
+
+print("Roses are "+color)
+print(plural_noun+" are blue")
+print("I love "+celebrity)
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+Enter a Color: white
+Enter a Plural Noun: bedsheets
+Enter a Celebrity: song kang
+Roses are white
+bedsheets are blue
+I love song kang
+
+Process finished with exit code 0
