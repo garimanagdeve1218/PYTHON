cfff96e51fb0ce430eba5a844a452a4fc4900c67
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-24 00:20:35 +0530

    Create 7. Basic Calculator.py

---
+++ 7. Basic Calculator.py
@@ -0,0 +1,14 @@
+#to take two inputs from user and perform addition
+
+a=int(float("Enter the first number: "))
+b=float(input("Enter the second number: "))
+
+result = a + b
+print(result)
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+Enter the first number: 3
+Enter the second number: 4
+7
+
+Process finished with exit code 0
