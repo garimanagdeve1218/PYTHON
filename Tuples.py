375e567a377140f87877bc03d05e169e7e3827ce
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-30 23:55:52 +0530

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -5,4 +5,25 @@ print (coordinates[1])
 
 "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
 5
+Process finished with exit code 0
+
+#tuples can't be modified:
+
+coordinates = (4,5)
+coordinates[1] = 10
+print(coordinates[1])
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+Traceback (most recent call last):
+  File "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py", line 199, in <module>
+    coordinates[1] = 10
+    ~~~~~~~~~~~^^^
+TypeError: 'tuple' object does not support item assignment
+
+Process finished with exit code 1
+
+
+
+
+
 
