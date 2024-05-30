bc8f52752578d74c4d82d476ca834fca775e3020
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-31 00:20:26 +0530

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -13,14 +13,20 @@ coordinates = (4,5)
 coordinates[1] = 10
 print(coordinates[1])
 
-"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
-Traceback (most recent call last):
-  File "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py", line 199, in <module>
-    coordinates[1] = 10
-    ~~~~~~~~~~~^^^
-TypeError: 'tuple' object does not support item assignment
+#output: there will be an error
 
-Process finished with exit code 1
+
+
+#so basically a list works the same way but i can modify the value here:
+
+coordinates = [4,5]
+coordinates[1] = 10
+print(coordinates[1])
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+10
+
+Process finished with exit code 0
 
 
 
