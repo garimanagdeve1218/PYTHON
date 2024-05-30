87a79173264edabc39cd00fa8a765cc509c27fff
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-05-30 23:53:08 +0530

    Create Tuples.py

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+#tuples are immutable- can't be changed once made.
+
+coordinates = (4,5)
+print (coordinates[1])
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+5
+
