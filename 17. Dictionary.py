0f1ce0b7efe9d351a68b4869ea5e3977a9dc3bd5
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-06-03 02:38:33 +0530

    Create 17. Dictionary.py

---
+++ 17. Dictionary.py
@@ -0,0 +1,77 @@
+#Dictionary:
+
+#we want to build a program that can conver jan-->january and so on with all the months:
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthConversions["Nov"])
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+November
+
+Process finished with exit code 0
+
+
+
+
+#another way:
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthConversions.get("Dec"))
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+December
+
+Process finished with exit code 0
+
+
+
+
+#we can even pass a default value for some key which is actually not present in the dictionary:
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthConversions.get("Doc","not a valid key!"))
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+not a valid key!
+
+Process finished with exit code 0
