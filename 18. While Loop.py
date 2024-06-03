ffc06e07f89d7017cbdb6f03eb3371b0586769ec
GARIMA NAGDEVE <144271162+garimanagdeve1218@users.noreply.github.com>
2024-06-03 21:31:08 +0530

    Create 18. While Loop.py

---
+++ 18. While Loop.py
@@ -0,0 +1,27 @@
+#while loop
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+print("Done with loop")
+
+"C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\.venv\Scripts\python.exe" "C:\Users\Garima Nagdeve\PycharmProjects\pythonProject\app.py" 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Done with loop
+
+Process finished with exit code 0
+
+
+
+
